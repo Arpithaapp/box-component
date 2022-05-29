8290fa48c1b6a2f76cf74322598f85a44380d4d7
Arpithaapp <95266953+Arpithaapp@users.noreply.github.com>
2022-05-29 10:24:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-//const colors = ['yellow', 'green', 'blue', '#f15025', 'rgba(125,125,125,0.5)'];
+
 
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function onClick(event) {
     event.target.style.backgroundColor = 'red'
-//   let random = Math.floor(Math.random() * colors.length);
-//   let buttonColor = 1;
-//   if(random==4){
-//     buttonColor = random - 1;
-//   } else {
-//     buttonColor = random + 1;
-//   }
     
 })
